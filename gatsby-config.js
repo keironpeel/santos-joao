c9cb60f553c9d401ff4fc1be68414742ad53b81a
keironpeel <peelkeiron@gmail.com>
2021-06-22 10:04:42 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "santos-joao",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+  ],
+};
