7875b27647aaa81e7f0b39e6609eb654697a799d
keironpeel <peelkeiron@gmail.com>
2021-07-12 16:58:34 +0100

    Added Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,16 @@ module.exports = {
   siteMetadata: {
     title: "santos-joao",
   },
-  plugins: ["gatsby-plugin-react-helmet", "gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sass",
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "G-CWED9V677T",
+        head: false,
+        enableWebVitalsTracking: true,
+      },
+    },
+  ],
 };
